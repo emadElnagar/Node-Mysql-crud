6ca2f06f4a3af94ff5b8f2fcaa2015fff6b65cb1
unknown <emad.m.alnagar@gmail.com>
2023-01-30 04:40:32 +0200

    Connecting database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { createConnection } from 'mysql';
 
 const app = express();
 const port = process.env.PORT || 5000;
+const dbPassword = process.env.PASSWORD || '';
+const databaseSrc = process.env.DBSRC || 'company';
+
+const db = createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: dbPassword,
+  database: databaseSrc
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
