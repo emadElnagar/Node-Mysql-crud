537aaed7124b2d90be7c59e5593170e75829ac27
unknown <emad.m.alnagar@gmail.com>
2023-02-06 16:34:03 +0200

    modify navbar style

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <ul className="flex justify-evenly">
-        <li className="px-5 py-2 capitalize transition ease-in-out delay-300 hover:text-sky-400 cursor-pointer">
+    <nav className="h-16 shadow-lg">
+      <ul className="h-full flex justify-evenly">
+        <li className="flex flex-col justify-center capitalize transition ease-in-out duration-300 hover:text-sky-400 cursor-pointer">
           <Link to={`/`}>home</Link>
         </li>
-        <li className="px-5 py-2 capitalize transition ease-in-out delay-300 hover:text-sky-400 cursor-pointer">
+        <li className="flex flex-col justify-center capitalize transition ease-in-out duration-300 hover:text-sky-400 cursor-pointer">
         <Link to={`/new`}>new book</Link>
         </li>
       </ul>
