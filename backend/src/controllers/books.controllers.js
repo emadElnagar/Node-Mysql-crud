a879e5dfc8946147ab399f403274cd5609ad7637
unknown <emad.m.alnagar@gmail.com>
2023-02-08 14:46:49 +0200

    Create new book backend

--- backend/src/controllers/books.controllers.js
+++ backend/src/controllers/books.controllers.js
@@ -8,3 +8,13 @@ export const getAllBooks = (_req, res) => {
     res.send(result);
   });
 }
+
+export const newBook = (req, res) => {
+  const inserQuery = `INSERT INTO books (title, author) VALUES (?, ?)`;
+  db.query(inserQuery, [title, author], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  })
+}
--- backend/src/routes/books.routes.js
+++ backend/src/routes/books.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAllBooks } from '../controllers/books.controllers.js';
+import { getAllBooks, newBook } from '../controllers/books.controllers.js';
 
 const booksRouter = express.Router();
 
 booksRouter.get('/books', getAllBooks);
+
+booksRouter.post('/books/new', newBook);
 
 export default booksRouter;
