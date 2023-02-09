4fd9ec5369cbb3e97230b836847eeb5160755723
unknown <emad.m.alnagar@gmail.com>
2023-02-09 21:37:03 +0200

    Navigate

--- frontend/src/Pages/FormPage.js
+++ frontend/src/Pages/FormPage.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const FormPage = () => {
+  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const handleSubmit = (e) => {
@@ -11,14 +13,14 @@ const FormPage = () => {
       id,
       title,
       author
-    });
+    }).then(navigate('/'));
   }
   return (
     <div className="w-9/12 m-auto my-14 uppercase">
       <h1 className="font-serif font-bold text-3xl text-center">create new book</h1>
       <form className="mt-5" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Book title" className="w-full mt-4 px-5 py-2 border border-solid border-1 border-sky-400 focus:outline-none" onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Book author" className="w-full mt-4 px-5 py-2 border border-solid border-1 border-sky-400 focus:outline-none" onChange={(e) => setAuthor(e.target.value)} />
+        <input type="text" placeholder="Book title" className="w-full mt-4 px-5 py-2 border border-solid border-1 border-sky-400 focus:outline-none" onChange={(e) => setTitle(e.target.value)} required />
+        <input type="text" placeholder="Book author" className="w-full mt-4 px-5 py-2 border border-solid border-1 border-sky-400 focus:outline-none" onChange={(e) => setAuthor(e.target.value)} required />
         <input
           type="submit"
           value="submit"
