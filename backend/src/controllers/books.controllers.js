be2f6f3629ac13220d54ae79956e8861e7ba02b3
unknown <emad.m.alnagar@gmail.com>
2023-02-11 19:48:37 +0200

    Slugify book title

--- backend/src/controllers/books.controllers.js
+++ backend/src/controllers/books.controllers.js
@@ -1,5 +1,7 @@
-import db from "../utils/db.js"
+import db from "../utils/db.js";
+import slugify from 'slugify';
 
+// Get All Books Controller
 export const getAllBooks = (_req, res) => {
   db.query(`SELECT * FROM books`, (err, result) => {
     if (err) {
@@ -9,10 +11,16 @@ export const getAllBooks = (_req, res) => {
   });
 }
 
+// Create New Book Controller
 export const newBook = (req, res) => {
   const { id, title, author } = req.body;
-  const inserQuery = `INSERT INTO books (id, title, author) VALUES (?, ?, ?)`;
-  db.query(inserQuery, [id, title, author], (err, result) => {
+  const slug = slugify(title, {
+    replacement: '-',
+    lower: true,
+    strict: true,
+  });
+  const inserQuery = `INSERT INTO books (id, title, author, slug) VALUES (?, ?, ?, ?)`;
+  db.query(inserQuery, [id, title, author, slug], (err, result) => {
     if (err) {
       console.log(err);
     }
--- backend/src/routes/books.routes.js
+++ backend/src/routes/books.routes.js
@@ -3,8 +3,10 @@ import { getAllBooks, newBook } from '../controllers/books.controllers.js';
 
 const booksRouter = express.Router();
 
+// Get All Books Route
 booksRouter.get('/', getAllBooks);
 
+// Create New Book Route
 booksRouter.post('/new', newBook);
 
 export default booksRouter;
