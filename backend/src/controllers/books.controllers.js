d584a8a6df761cb601785b3a16b0e568c52a739c
unknown <emad.m.alnagar@gmail.com>
2023-02-13 20:23:35 +0200

    Unique title

--- backend/src/controllers/books.controllers.js
+++ backend/src/controllers/books.controllers.js
@@ -20,10 +20,23 @@ export const newBook = (req, res) => {
     strict: true,
   });
   const inserQuery = `INSERT INTO books (id, title, author, slug) VALUES (?, ?, ?, ?)`;
-  db.query(inserQuery, [id, title, author, slug], (err, result) => {
+  const findTitle = `SELECT * FROM books WHERE title = (?);`;
+  db.query(findTitle, [title], (err, result) => {
     if (err) {
       console.log(err);
     }
-    res.send(result);
-  });
+    if (result.length > 0) {
+      res.status(401).json({
+        message: 'This Book already found in your library'
+      });
+    } else {
+      db.query(inserQuery, [id, title, author, slug], (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        res.send(result);
+      });
+    }
+  })
+  
 }
