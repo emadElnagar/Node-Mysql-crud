fbdc121225a3a69ff0af89c9089f8eb3204674f2
unknown <emad.m.alnagar@gmail.com>
2023-03-01 20:27:37 +0200

    Get all books

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,30 @@
+import { useState, useEffect  } from "react";
 import BookCard from "../components/BookCard";
+import axios from "axios";
 
 const HomePage = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/books').then((books) => {
+      setList(books.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, [list]);
   return (
-    <BookCard />
+    <div>
+      {
+        list.length > 0 ? list.map((book) => (
+          <BookCard key={book.id}  book={book} />
+        )) : (
+          <div className="h-custom flex justify-center items-center">
+            <h1 className="font-serif capitalize text-2xl text-red-600">there is no books yet</h1>
+          </div>
+        )
+      }
+    </div>
   )
 }
+// <BookCard />
 
 export default HomePage;
--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -1,8 +1,9 @@
-const BookCard = () => {
+const BookCard = (props) => {
+  const { book } = props;
   return (
     <div className="shadow-md w-96 min-w-96 m-auto my-14 py-10 text-center">
-      <h1 className="font-serif capitalize text-2xl text-sky-400">book title</h1>
-      <p className="font-serif capitalize text-lg mt-5">by: <span>emad elnagar</span></p>
+      <h1 className="font-serif capitalize text-2xl text-sky-400">{ book.title }</h1>
+      <p className="font-serif capitalize text-lg mt-5">by: <span>{ book.author }</span></p>
     </div>
   )
 }
