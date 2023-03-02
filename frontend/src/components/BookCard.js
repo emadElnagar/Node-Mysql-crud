194104e5609b1b19796eb087ed0bccba48c94f95
unknown <emad.m.alnagar@gmail.com>
2023-03-02 21:39:02 +0200

    Edit and delete buttons

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -1,9 +1,16 @@
+import { AiOutlineDelete } from "react-icons/ai";
+import { FiEdit2 } from "react-icons/fi";
+
 const BookCard = (props) => {
   const { book } = props;
   return (
-    <div className="shadow-md w-96 min-w-96 m-auto my-14 py-10 text-center">
+    <div className="shadow-md w-96 min-w-96 m-auto my-14 pt-8 text-center">
       <h1 className="font-serif capitalize text-2xl text-sky-400">{ book.title }</h1>
       <p className="font-serif capitalize text-lg mt-5">by: <span>{ book.author }</span></p>
+      <div className="flex justify-between p-8">
+        <AiOutlineDelete className="cursor-pointer text-red-600" title="Delete" />
+        <FiEdit2 className="cursor-pointer text-violet-600" title="Edit" />
+      </div>
     </div>
   )
 }
